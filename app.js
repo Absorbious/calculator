b987a918c978766b6672eaa02d445daaa72fe02d
Adnan Jusic <adnanjusic98@gmail.com>
2022-03-16 20:37:05 +0100

    Finished calculator logic, added error message

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-let displaySpan = document.querySelector("span");
+let display = document.querySelector("span");
 let btn = document.querySelectorAll("button");
-let display = 0;
 let button;
 let num1 = 0;
 let num2 = 0;
@@ -9,38 +8,16 @@ let userOperator;
 // Need to use ForEach loop on the btn because querySelectorAll returns a nodeList
 btn.forEach((but) =>
   but.addEventListener("click", function (e) {
-    // Adding numbers to display and assigning it to a value
-    if (e.target.parentElement.className === "numbers") {
-      displaySpan.innerText += e.target.innerText;
-      display = parseInt(displaySpan.innerText);
-    } else if (e.target.innerText === "Clear") {
-      clear();
-      console.log("cleared");
-    } else if (e.target.parentElement.className === "operators" && e.target.className !== "equals") {
-      if (display === 0 && userOperator === "/") {
-        alert("Can't operate with 0");
-      } else if(num1 != 0){
-
-      } else {
-        num1 = display;
-        userOperator = e.target.innerText;
-        console.log(userOperator);
-      }
-    } else if(e.target.className === "equals"){
-
-    }
+    updateVal(e);
   })
 );
 
 function clear() {
-  display = 0;
-  displaySpan.innerText = "";
+  display.innerText = "";
   num1 = 0;
   num2 = 0;
-  userOperator = '';
+  userOperator = "";
 }
-
-// Kad se pritisne neki operator, prvi broj se uzme u funkciju i kada se pritisne tipka 'jednako', u funkciju operate se uzmu oba broja i operator i izvrsi se matematicka jednadzba ciji se rezultat prikaze nakon toga na displayu
 
 function add(a, b) {
   return a + b;
@@ -70,5 +47,46 @@ function operate(operator, a, b) {
 
     default:
       return;
+  }
+}
+
+// Function for displaying numbers and assigning them to a value
+function updateDisplay(e) {
+  if (e.target.parentElement.className === "numbers") {
+    // Displays number that's been clicked, if clicked after an operator, it is displayed instead of the first value
+    if (display.innerText == num1) {
+      display.innerText = "";
+    }
+    display.innerText += e.target.innerText;
+  } else if (e.target.innerText === "C") {
+    // Resets the calculator
+    clear();
+  } else if (
+    e.target.parentElement.className === "operators" &&
+    e.target.className !== "equals"
+  ) {
+    // When an operator is clicked, this assigns value of the display to the first variable
+    userOperator = e.target.innerText;
+    num1 = parseInt(display.innerText);
+    userOperator = e.target.innerText;
+    console.log(userOperator);
+  } else if (e.target.className === "equals") {
+    // When equal is clicked, it assigns the display value to the second variable, erases the display and calls the operation function which displays the result
+    // If tried to divide by 0, display returns an error
+    num2 = parseInt(display.innerText);
+    display.innerText = "";
+    if (userOperator === "/" && (num1 === 0 || num2 === 0)) {
+      display.innerText = "Can't divide by 0!";
+    } else {
+      display.innerText = operate(userOperator, num1, num2);
+    }
+  }
+}
+
+function updateVal(e) {
+  if (num1 === 0) {
+    updateDisplay(e);
+  } else {
+    updateDisplay(e);
   }
 }
